83f09668f30229d8dc57621963e163fe58782ef8
Umamaheswara Reddy <umamaheswar.career@gmail.com>
2025-07-31 09:06:05 +0530

    sass refactored

--- e-commerce-web/src/app/header/header.component.ts
+++ e-commerce-web/src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormField } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatFormField],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
