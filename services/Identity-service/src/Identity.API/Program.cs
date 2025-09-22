d61dd6205e8d3db660fb24d252486ced46578289
Umamaheswara Reddy <umamaheswar.career@gmail.com>
2025-09-22 20:56:16 +0530

    Project restructured

---
+++ services/Identity-service/src/Identity.API/Program.cs
@@ -0,0 +1,38 @@
+using Identity.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("IdentityDb")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => { /* configure later */ });
+
+builder.Services.AddAuthorization();
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapGet("/", () => "Identity API is running!");
+
+app.Run();
---
+++ services/Identity-service/src/Identity.Infrastructure/Data/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Infrastructure.Data;
+
+public class ApplicationUser : IdentityUser
+{
+    // Add any custom fields here
+}
+
+public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+{
+    public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+        : base(options) { }
+}
+
