6d9a46fa64819144e52d9a777316052c3d18c915
Umamaheswara Reddy <umamaheswar.career@gmail.com>
2025-11-01 11:16:48 +0530

    code refactored

--- web/src/app/ui/directives/control-value-accessor.directive.ts
+++ web/src/app/ui/directives/control-value-accessor.directive.ts
@@ -1,26 +1,41 @@
-import { computed, DestroyRef, Directive, Inject, Injector, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  computed,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  FormControlDirective,
+  FormControlName,
+  FormGroupDirective,
+  NgControl,
+  Validators,
+} from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ControlValueAccessor, FormControl, FormControlDirective, FormControlName, FormGroupDirective, NgControl, Validators } from '@angular/forms';
 import { startWith, distinctUntilChanged, tap } from 'rxjs';
 
 @Directive({
   selector: '[appControlValueAccessor]'
 })
-export class ControlValueAccessorDirective<T> implements ControlValueAccessor, OnInit {
+export class ControlValueAccessorDirective<T>
+  implements ControlValueAccessor, OnInit {
 
-  private control?: FormControl | undefined;
-  isRequired = false;
-  isDisabled: boolean = false;
+  private readonly formGroupDir = inject(FormGroupDirective, { optional: true });
+  private readonly destroyRef = inject(DestroyRef);
+    
+  protected readonly ngControl = inject(NgControl, { optional: true });
+  protected control?: FormControl;
   protected value: T | null = null;
+  isRequired = false;
+  isDisabled = false;
 
-  // Callback functions
   private onChange: (value: any) => void = () => {};
   private onTouched: () => void = () => {};
 
-  // computed form control
   controlSig = computed(() => this.control);
-
-  constructor(@Inject(Injector) private injector: Injector) {}
 
   ngOnInit(): void {
     this.setFormControl();
@@ -28,46 +43,41 @@ export class ControlValueAccessorDirective<T> implements ControlValueAccessor, O
   }
 
   private setFormControl() {
-    const formControl = this.injector.get(NgControl);
     try {
-      switch (formControl.constructor) {
-        case FormControlName: {
-          this.control = this.injector.get(FormGroupDirective).getControl(formControl as FormControlName);
-          break;
-        }
-        default:
-          this.control = (formControl as FormControlDirective).form as FormControl;
+      if (this.ngControl instanceof FormControlName && this.formGroupDir) {
+        this.control = this.formGroupDir.getControl(this.ngControl);
+      } else if (this.ngControl instanceof FormControlDirective) {
+        this.control = this.ngControl.form as FormControl;
       }
-    }
-    catch { }
+    } catch {}
   }
 
   writeValue(value: T): void {
     this.value = value;
   }
 
-  registerOnChange(fn: (val: T | null) => T): void {
+  registerOnChange(fn: (val: T | null) => void): void {
     this.controlSig()?.valueChanges
-      .pipe(takeUntilDestroyed(this.injector.get(DestroyRef)),
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
         startWith(this.control?.value),
         distinctUntilChanged(),
-        tap((val: T)=> fn(val))
-      ).subscribe(() => this.controlSig()?.markAsUntouched());
+        tap((val: T) => fn(val))
+      )
+      .subscribe(() => this.controlSig()?.markAsUntouched());
     this.onChange = fn;
   }
 
-  registerOnTouched(fn: () => T): void {
+  registerOnTouched(fn: () => void): void {
     this.onTouched = fn;
   }
 
   onBlur() {
     this.onTouched();
-    console.log('blurred');
   }
 
   onInput(event: Event) {
-    if(this.isDisabled)
-      return;
+    if (this.isDisabled) return;
     this.value = (event.target as HTMLInputElement).value as unknown as T;
     this.onChange(this.value);
     this.onTouched();
@@ -81,5 +91,4 @@ export class ControlValueAccessorDirective<T> implements ControlValueAccessor, O
     const control = this.control;
     return !!(control && control.invalid && (control.dirty || control.touched));
   });
-
 }
